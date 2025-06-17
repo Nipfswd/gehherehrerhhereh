770348f86ea9e0f1c1cd52aed8205a5c955bd5c9
NoahBajsToa <166116811+Nipfswd@users.noreply.github.com>
2025-06-17 23:25:27 +0200

    Add files via upload

---
+++ lua.lua
@@ -0,0 +1,9 @@
+-- Simple Lua script for a-Shell
+
+print("Hello from Lua in a-Shell")
+
+-- Ask for user input
+io.write("Enter your name: ")
+local name = io.read()
+
+print("Nice to träffa dig din rotta, " ..name .. "!")
